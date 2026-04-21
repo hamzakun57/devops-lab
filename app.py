879f31cecc125299a51629f2468edf8b40e7c462
hamza kaissouni <hamzakaissouni5@email.com>
2026-04-21 21:09:03 +0000

    version 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Bravo Hamza kaissouni ! Pipeline CI/CD Operationnel"
+    return "Bravo Hamza kaissouni ! Pipeline est terminer"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
